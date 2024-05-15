09c59366b2c9af0e010777318ff1748b0618b0ea
irenecapitano <irene.capitano@kinkaid.org>
2024-05-15 11:24:04 -0500

    on wednesday may 15th, 2024 i worked on creating the layout of my website and making it a lot better looking. i am working on setting up the json and combining that with my javascript. im hoping to add the buttons on the blurred iceskating image that i have right now

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const members;
+
 function runProgram(){
 
     console.log('runProgram');
     //your code goes here
+}
+
+async fuction getPlayers(){
+    let response = await fetch("data.json");
+    console.log(response);
+    let data = response.json();
 }
 document.addEventListener('DOMContentLoaded', runProgram);
